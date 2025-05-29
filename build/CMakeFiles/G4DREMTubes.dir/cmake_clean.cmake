b210297fedcfbe7c7709201b748a26c71155f14b
AndreaPareti <apareti@saphira.pv.infn.it>
2025-05-29 14:38:29 +0200

    Remove build/ from repo and add to .gitignore

--- build/CMakeFiles/G4DREMTubes.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/G4DREMTubes"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/G4DREMTubes.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
